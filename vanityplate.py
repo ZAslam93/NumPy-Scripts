ddfbe4f10c8cc95d4f10a097a9fa95c62491315c
Zubul <Zubair.aslam93@yahoo.com>
2024-11-26 12:53:25 -0600

    uhh

--- vanityplate.py
+++ vanityplate.py
@@ -15,21 +15,36 @@ def validatePlate(plate):
     midpoint = calculateMidPoint(plateLength)
     firstdigit = 0
     print(f'Midpoint | {midpoint}')
-    print(midpoint)
+
+    # Checking if the first two digits are alpha
     if plate[0].isdigit() or plate[1].isdigit():
         print("All vanity plates must start with at least two letters.")
         return False
+    
+    for i in plate: 
+         if not i.isalpha() and not i.isdigit(): 
+              # Iterate across the string and return False if any special chars are found
+              print("Punctuation (spaces, periods, or other special characters) are invalid")
+              return False
+         
+    # Using the midpoint function to check if the midpoint of the string is a number
     if plate[midpoint].isdigit(): 
             print("Numbers cannot be used in the middle of a plate")
             return False
+    
+    # Iterating through the string until the first number is found, if any
     for i in range(plateLength):
          if plate[i].isdigit(): 
               firstdigit = int(i)
+              break
     
+    # Using the located index to check if it's nonzero 
     if plate[firstdigit] == 0: 
          print("The first number cannot be a zero")
          return False
     
+    # If a number does occur in the plate, the plate must end with a digit
+    # plateLength - 1 to account for zero indexing. plateLength - 1 means the last character of the string
     if firstdigit > 0 and plate[plateLength - 1].isalpha(): 
          print("Numbers must come at the end of the plate")
          return False
@@ -37,11 +52,16 @@ def validatePlate(plate):
     return True
 
 def calculateMidPoint(length: int):
+     # If the string has an even number of characters, take the floored value - 1
      if length % 2 == 0:
           return (length // 2) - 1
-     elif length == 0: 
+     elif length <= 0: 
         print('Provide a positive integer value')
-        return False
+        return -1 
+     
+     # If the string has an odd number of characters, take the floored value as is
+     else:
+          return length // 2
 checkPlates()
 
 # “All vanity plates must start with at least two letters.”
